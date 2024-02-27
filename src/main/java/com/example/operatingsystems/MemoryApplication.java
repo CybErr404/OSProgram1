5ed8b2de33a2a3f50b4b85250d390215ff8e59e4
CybErr404 <wattsm6@go.stockton.edu>
2024-02-27 14:54:07 -0500

    Created beginning of memory allocation program

---
+++ src/main/java/com/example/operatingsystems/MemoryApplication.java
@@ -0,0 +1,22 @@
+package com.example.operatingsystems;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class MemoryApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(OperationsApplication.class.getResource("memory-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 660, 470);
+        stage.setTitle("Expression Evaluator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/operatingsystems/MemoryView.java
@@ -0,0 +1,10 @@
+package com.example.operatingsystems;
+
+public class MemoryView {
+    private int X; //fixed partitioning memory size
+    private int Y; //dynamic partitioning memory size
+    private String Z; //string address
+
+
+
+}
