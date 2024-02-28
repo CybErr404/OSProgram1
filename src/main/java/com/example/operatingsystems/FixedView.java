996b4151a8beac3491f3d634c2edfaebc2f5d061
CybErr404 <wattsm6@go.stockton.edu>
2024-02-28 09:34:27 -0500

    Finished fixed first-fit method

--- src/main/java/com/example/operatingsystems/FixedView.java
+++ src/main/java/com/example/operatingsystems/FixedView.java
@@ -15,14 +15,16 @@ import java.util.Arrays;
 
 public class FixedView {
     private int X; //fixed partitioning memory size
-
     private StringBuilder answerString;
+    private int memorySum;
 
     private int fragmentationValue; // holds the total fragmentation value for each allocation scheme
     private ArrayList<Integer> fixedJobSizeArray; // holds job sizes for fixed memory allocation
     private ArrayList<Integer> fixedMemoryRequirementArray; // holds memory requirements for fixed memory allocation
 
     private ArrayList<Integer> memoryAddresses; // holds memory addresses for jobs
+
+    private ArrayList<String> memoryList;
 
     // creation of all buttons related to fixed memory allocation.
     @FXML
@@ -47,9 +49,10 @@ public class FixedView {
         fragmentationValue = 0;
         answerString = new StringBuilder("");
 
-        fixedJobSizeArray = new ArrayList<>(Arrays.asList(2500, 250, 100, 50, 150));
+        fixedJobSizeArray = new ArrayList<>(Arrays.asList(2500, 250, 100, 600, 150));
         fixedMemoryRequirementArray = new ArrayList<>(Arrays.asList(3500, 2500, 1500, 500, 2000));
         memoryAddresses = new ArrayList<>(Arrays.asList(10, 25, 150, 225, 300));
+        memoryList = new ArrayList<>();
     }
 
     @FXML
@@ -58,15 +61,20 @@ public class FixedView {
             for(int j = 0; j < fixedMemoryRequirementArray.size(); j++) {
                 if (fixedJobSizeArray.get(i) <= fixedMemoryRequirementArray.get(j)) {
                     answerString.append("Job ").append(i).append(" has been placed in Memory Location ")
-                            .append(j).append(". ");
+                            .append(j).append(".\n");
                     fragmentationValue = fragmentationValue + (fixedMemoryRequirementArray.get(j) -
                             fixedJobSizeArray.get(i));
+                    memoryList.add(String.valueOf(fixedMemoryRequirementArray.get(i)));
+                    memoryList.add(String.valueOf(memoryAddresses.get(i)));
+                    memoryList.add("Job " + i);
+                    memoryList.add("Busy");
+                    answerString.append(memoryList).append("\n");
                     fixedMemoryRequirementArray.set(j, 0);
                     break;
                 }
-                else if (fixedJobSizeArray.get(i) > fixedMemoryRequirementArray.get(j)) {
-                    answerString.append("Job ").append(i).append(" has to wait! ");
-                }
+//                else if (fixedJobSizeArray.get(i) > fixedMemoryRequirementArray.get(j)) {
+//                    answerString.append("Job ").append(i).append(" has to wait! ");
+//                }
             }
         }
         answerString.append("\nFragmentation: ").append(fragmentationValue);
