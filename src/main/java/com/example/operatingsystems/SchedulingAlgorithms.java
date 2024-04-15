d0a37a11c11b79d02440037aabdf2a6909d4fd9b
CybErr404 <wattsm6@go.stockton.edu>
2024-04-15 15:11:29 -0400

    Edited methods

--- src/main/java/com/example/operatingsystems/SchedulingAlgorithms.java
+++ src/main/java/com/example/operatingsystems/SchedulingAlgorithms.java
@@ -36,12 +36,14 @@ public class SchedulingAlgorithms {
     public void shortestJobNext(ArrayList<String> jobs, ArrayList<Integer> arrivals,
                                   ArrayList<Integer> cycles) {
         double waitingTime = 0.0;
-        double turnaroundTime = 0.0;
+        double turnaroundTime;
+        double finishTime = 0.0;
         ArrayList<Double> turnaroundTimesArray = new ArrayList<>();
-        ArrayList<Double> waitTimesArray = new ArrayList<>();
         System.out.printf("%-15s %-20s %-15s", "Job", "Waiting Time", "Turnaround Time\n");
         for(int i = 0; i < jobs.size(); i++) {
-            turnaroundTime = (turnaroundTime + arrivals.get(i)) - 0.0;
+            turnaroundTime = 0.0;
+            finishTime = finishTime + cycles.get(i);
+            turnaroundTime = finishTime - arrivals.get(i);
             turnaroundTimesArray.add(turnaroundTime);
             System.out.printf("%-15s %-20.1f %-15.1f", jobs.get(i), waitingTime, turnaroundTime);
             System.out.println();
@@ -61,13 +63,17 @@ public class SchedulingAlgorithms {
     public void shortestRemainingTime(ArrayList<String> jobs, ArrayList<Integer> arrivals,
                                         ArrayList<Integer> cycles) {
         double waitingTime = 0.0;
-        double turnaroundTime = 0.0;
+        double turnaroundTime;
+        double finishTime = 0.0;
         ArrayList<Double> turnaroundTimesArray = new ArrayList<>();
-        System.out.printf("%-15s %-15s", "Waiting Time", "Turnaround Time");
+        System.out.printf("%-15s %-20s %-15s", "Job", "Waiting Time", "Turnaround Time\n");
         for(int i = 0; i < jobs.size(); i++) {
-            waitingTime = 0.0;
             turnaroundTime = 0.0;
-            System.out.printf("%-15.2f %-15.2f", waitingTime, turnaroundTime);
+            finishTime = finishTime + cycles.get(i);
+            turnaroundTime = finishTime - arrivals.get(i);
+            turnaroundTimesArray.add(turnaroundTime);
+            System.out.printf("%-15s %-20.1f %-15.1f", jobs.get(i), waitingTime, turnaroundTime);
+            System.out.println();
         }
 
         double averageWaitingTime = 0.0;
@@ -84,13 +90,17 @@ public class SchedulingAlgorithms {
     public void roundRobin(ArrayList<String> jobs, ArrayList<Integer> arrivals,
                              ArrayList<Integer> cycles) {
         double waitingTime = 0.0;
-        double turnaroundTime = 0.0;
+        double turnaroundTime;
+        double finishTime = 0.0;
         ArrayList<Double> turnaroundTimesArray = new ArrayList<>();
-        System.out.printf("%-15s %-15s", "Waiting Time", "Turnaround Time");
+        System.out.printf("%-15s %-20s %-15s", "Job", "Waiting Time", "Turnaround Time\n");
         for(int i = 0; i < jobs.size(); i++) {
-            waitingTime = 0.0;
             turnaroundTime = 0.0;
-            System.out.printf("%-15.2f %-15.2f", waitingTime, turnaroundTime);
+            finishTime = finishTime + cycles.get(i);
+            turnaroundTime = finishTime - arrivals.get(i);
+            turnaroundTimesArray.add(turnaroundTime);
+            System.out.printf("%-15s %-20.1f %-15.1f", jobs.get(i), waitingTime, turnaroundTime);
+            System.out.println();
         }
 
         double averageWaitingTime = 0.0;
