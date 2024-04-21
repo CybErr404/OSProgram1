69e04fdf4caa2c2b25df85886055fb37d3b9328e
CybErr404 <wattsm6@go.stockton.edu>
2024-04-21 16:57:25 -0400

    Added method connections

--- src/main/java/com/example/operatingsystems/SchedulingView.java
+++ src/main/java/com/example/operatingsystems/SchedulingView.java
@@ -1,4 +1,45 @@
 package com.example.operatingsystems;
 
+//Import statements for the buttons, labels, and the JavaFX application.
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * @author Mia Watts
+ * This is the method class for the scheduling algorithm program. It contains variables that
+ * represent the labels and buttons which will be used in the program and has each of the four
+ * scheduling algorithms programmed and defined within.
+ */
 public class SchedulingView {
+    @FXML
+    private Label answerLabel;
+    @FXML
+    private Button fcfsButton;
+    @FXML
+    private Button sjnButton;
+    @FXML
+    private Button srtButton;
+    @FXML
+    private Button roundRobinButton;
+
+    @FXML
+    protected void onFCFSClick() {
+
+    }
+
+    @FXML
+    protected void onSJNClick() {
+
+    }
+
+    @FXML
+    protected void onSRTClick() {
+
+    }
+
+    @FXML
+    protected void onRoundRobinClick() {
+
+    }
 }
